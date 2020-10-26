bb3bd1c2f4bcf5eb60e70153ba5f6c86dd539e38
Łukasz Pawela <lukasz.pawela@gmail.com>
2020-10-26 01:27:49 +0100

    first commit

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+haml about.haml about.html
+haml authors.haml authors.html
+haml bibliography.haml bibliography.html
+haml bulk.haml bulk.html
+haml buy.haml buy.html
+haml images.haml images.html
+haml index.haml index.html
+haml introduction.haml introduction.html
+haml media.haml media.html
+haml mural.haml mural.html
+haml toc.haml toc.html
+haml trivia.haml trivia.html
